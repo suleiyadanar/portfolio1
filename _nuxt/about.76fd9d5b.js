1dff74e74803ec0544a3f632e7253f1a954110c2
suleiyadanar <84138664+suleiyadanar@users.noreply.github.com>
2023-07-23 02:22:11 +0000

    Deploying to gh-pages from @ suleiyadanar/portfolio@5ead685436e41ecd522afa37d3f32fc3ea2fadee 🚀

--- _nuxt/about.76fd9d5b.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c,o}from"./entry.98d376c3.js";const t={};function n(r,a){return o(),c("h1",null,"About page")}const _=e(t,[["render",n]]);export{_ as default};
--- _nuxt/index.5452364f.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c,o as n}from"./entry.98d376c3.js";const o={};function r(t,a){return n(),c("h1",null,"Home page")}const _=e(o,[["render",r]]);export{_ as default};
